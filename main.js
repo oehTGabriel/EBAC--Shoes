b5dd19b0cfea37d24b892c097febbd46e56036fc
Theo Gabriel <theonbgabriel@gmail.com>
2024-10-31 19:14:22 -0300

    adicionando ao carrinho

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Abrir menu
+
 const carrinho = document.getElementById('carrinho');
 const botaoCarrinho = document.getElementById('botao-carrinho');
 const botaoFecharCarrinho = document.getElementById('botao-fechar-carrinho');
@@ -8,4 +10,26 @@ botaoCarrinho.addEventListener('click', () => {
 
 botaoFecharCarrinho.addEventListener('click', () => {
     carrinho.classList.remove('active');
+})
+
+//adicionar carrinho
+
+const botaoProduto = document.querySelectorAll('.produto button');
+const lista = document.querySelector('.compras ul');
+
+botaoProduto.forEach((botao) => {
+    botao.addEventListener("click", function () {
+        const produto = this.parentElement;
+
+        const preco = produto.querySelector('p').textContent;
+        const imagem = produto.querySelector('img').src;
+
+        const compraNoCarrinho = document.createElement('li');
+            compraNoCarrinho.innerHTML = ` <li>
+                    <img src="${imagem}" alt="">
+                    <p>${preco}</p>
+                </li>`;
+
+        lista.appendChild(compraNoCarrinho);
+    })
 })
